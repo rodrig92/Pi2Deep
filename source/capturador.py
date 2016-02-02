bd79c73db47d07eb8941db2f29032830d7001f4a
Rodrigo Colombo <rodrig92@yahoo.es>
2016-02-02 20:39:27 +0000

    Modificado el script para que la camara no quede siempre encendida a la espera, ahora se enciende en el momento

--- source/capturador.py
+++ source/capturador.py
@@ -6,7 +6,7 @@
 ###                reconocimiento de imagenes realizado     ###
 ###                por Pete Warden y la captura de imagen   ###
 ###                de la Raspberry Pi 2.                    ###
-###   Version:     V0.1                                     ###
+###   Version:     V1.2                                     ###
 ###############################################################
 
 import picamera
@@ -19,16 +19,16 @@ GPIO.setmode(GPIO.BCM)                                                     #sele
 GPIO.setup(17, GPIO.IN)                                                    #establecemos el pin GPIO17 como input   
    
 try:
-        camera = picamera.PiCamera()
-        if len(sys.argv) == 2:
-                camera.resolution = (int(sys.argv[1]), int(sys.argv[1]))   #caso de pasarle un tamanio por argumento
-        elif len(sys.argv) == 3:
-                camera.resolution = (int(sys.argv[1]), int(sys.argv[2]))   #caso de pasarle los tamanios por argumento
-        else:
-                camera.resolution = (600, 600)                             #tamanio por defecto de la imagen
-  
         while True:                                                        #bucle para la espera de pulsar el boton 
                 if (GPIO.input(17) == 1):
+                        camera = picamera.PiCamera()
+                        
+                        if len(sys.argv) == 2:
+                                camera.resolution = (int(sys.argv[1]), int(sys.argv[1]))   #caso de pasarle un tamanio por argumento
+                        elif len(sys.argv) == 3:
+                                camera.resolution = (int(sys.argv[1]), int(sys.argv[2]))   #caso de pasarle los tamanios por argumento
+                        else:
+                                camera.resolution = (600, 600)                             #tamanio por defecto de la imagen
                         print "captura"
                         name = str(camera.resolution)
                         name = str('img'+name+'.jpg')                      #asignacion del nombre de la imagen
@@ -38,12 +38,11 @@ try:
                         time.sleep(2)                                      #Tiempo para que saque la foto
 
                         ejecutar = ['./jpcnn','-i',name,'-n','../networks/jetpac.ntwk','-t','-m','s','-d']     #lista de parametros para la ejecucion del reconocimiento
-
                         call(ejecutar)                                     #ejecucion del reconocimiento
-
-                        time.sleep(2)                                      #retardo final
  
-                        call(['rm', name])                                 #borramos la foto resultado   
+                        call(['rm', name])                                 #borramos la foto resultado, se realiza al acabar el reconocimiento  
+                        time.sleep(1)
+                        camera.close()                                     #desactivamos la camara
 
 except KeyboardInterrupt:                                                  #CTRL+C para interrumpir  
         GPIO.cleanup()                                                     #reset de los puertos GPIO usados  
